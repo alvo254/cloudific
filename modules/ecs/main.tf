f5d1b4ae71f241e5eb4fdb556623e5e7f5c524c8
Alvin <alvinkhamron@gmail.com>
2024-06-10 23:34:14 +0300

    [update]

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -36,7 +36,24 @@ resource "aws_iam_role_policy_attachment" "ecs_cloudwatch_logs_policy" {
   role       = aws_iam_role.ecs_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
-
+locals {
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = "alvin254/fitnesshero"
+      cpu   = 256
+      memory = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort = 3000
+          protocol = "tcp"
+        }
+      ]
+    }
+  ])
+}
 resource "aws_ecs_task_definition" "cloudific" {
   family                = "app"
   network_mode          = "awsvpc"
@@ -44,7 +61,7 @@ resource "aws_ecs_task_definition" "cloudific" {
   execution_role_arn    = aws_iam_role.ecs_exec_role.arn
   cpu                      = "256"  // CPU value defined at task level
   memory                   = "512"  // Memory value defined at task level
-  container_definitions = file("${path.module}/container_definitions.json")
+  container_definitions = local.container_definitions
 }
 
 resource "aws_ecs_service" "app_service" {
@@ -60,28 +77,3 @@ resource "aws_ecs_service" "app_service" {
     assign_public_ip = true
   }
 }
-
-// {
-//   "family": "app",
-//   "networkMode": "awsvpc",
-//   "requiresCompatibilities": ["FARGATE"],
-//   "executionRoleArn": "arn:aws:iam::your_account_id:role/ecsExecRole",
-//   "cpu": "256",
-//   "memory": "512",
-//   "containerDefinitions": [
-//     {
-//       "name": "app",
-//       "image": "alvin254/fitnesshero",
-//       "cpu": 256,
-//       "memory": 512,
-//       "essential": true,
-//       "portMappings": [
-//         {
-//           "containerPort": 3000,
-//           "hostPort": 3000,
-//           "protocol": "tcp"
-//         }
-//       ]
-//     }
-//   ]
-// }
